54933f231cb6fa6e44f2640b8cfb2b63d483c56a
neelybd <34224624+neelybd@users.noreply.github.com>
2020-05-29 09:17:22 -0700

    1.0.0 - First full release of PCA    - Updated functions to use file_handling.py and selection.py    - Made PCA into function

--- BN_PCA.py
+++ BN_PCA.py
@@ -2,11 +2,14 @@ import pandas as pd
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename, asksaveasfilename
 from sklearn.decomposition import PCA
+from file_handling import *
+from selection import *
+
 
 def main():
     print("Program: PCA")
-    print("Release: 0.1.2")
-    print("Date: 2019-03-12")
+    print("Release: 0.2.0")
+    print("Date: 2020-05-29")
     print("Author: Brian Neely")
     print()
     print()
@@ -21,7 +24,7 @@ def main():
     file_in = select_file_in()
 
     # Set output file
-    file_out = select_file_out(file_in)
+    file_out = select_file_out_csv(file_in)
 
     # Ask for delimination
     delimination = input("Enter Deliminator: ")
@@ -35,8 +38,26 @@ def main():
     # Get list of columns from PCA
     columns = column_list(data, column_selection_type_input)
 
+    # Run PCA
+    data_out, components_df = bn_pca(data, columns)
+
+    # Write output file
+    print("Writing output file...")
+    data_out.to_csv(file_out, index=False)
+    print("Output file wrote!")
+
+    # Ask to export components
+    if y_n_question("Write PCA Component Loadings: "):
+        # Set components output file
+        components_file_out = select_file_out_csv(file_in)
+
+        # Write components CSV
+        components_df.to_csv(components_file_out)
+
+
+def bn_pca(data, pca_columns):
     # Get list of indices for complete data
-    data_complete_index = data.dropna(subset=columns).index
+    data_complete_index = data.dropna(subset=pca_columns).index
 
     # Drop na from original dataset
     data_na = data[~data.index.isin(data_complete_index)]
@@ -44,11 +65,11 @@ def main():
     # Get complete data
     data = data[data.index.isin(data_complete_index)]
 
+    # Set input data
+    x = data[pca_columns]
+
     # Set PCA factors
     pca = PCA()
-
-    # Set input data
-    x = data[columns]
 
     # Run PCA
     pca_results = pca.fit_transform(x)
@@ -76,7 +97,7 @@ def main():
     # Find unused columns in original dataset
     unused_columns = list()
     for i in list(data.columns.values):
-        if i not in columns:
+        if i not in pca_columns:
             unused_columns.append(i)
 
     # If number of factors is greater than number of actual factors, lower
@@ -98,8 +119,8 @@ def main():
     retain_pca_input = y_n_question("Retain columns used for PCA creation?: ")
 
     # If desired, concatenate pca data onto output file
-    if retain_pca_input == 'y':
-        data_out = pd.concat([data_out, data[columns]], axis=1)
+    if retain_pca_input:
+        data_out = pd.concat([data_out, data[pca_columns]], axis=1)
 
     # Union back null data making sure it exists in both datasets
     columns_to_append_lst = list()
@@ -111,52 +132,11 @@ def main():
     # Union back null data
     data_out = data_out.append(data_na[columns_to_append_lst], sort=False).sort_index()
 
-    # Write output file
-    print("Writing output file...")
-    data_out.to_csv(file_out, index=False)
-    print("Output file wrote!")
+    # Get PCA Components
+    components_df = pd.DataFrame(data=pca.components_, columns=pca_columns, index=pca_column_header)[:number_of_factors]
 
-    # Ask to export components
-    write_components = y_n_question("Write PCA Component Loadings: ")
-
-    if write_components == 'y':
-        components_file_out = select_file_out(file_in)
-        components_df = pd.DataFrame(data=pca.components_, columns=columns, index=pca_column_header)[:number_of_factors]
-        components_df.to_csv(components_file_out)
-
-
-def select_file_in():
-    file_in = askopenfilename(initialdir="../", title="Select file",
-                              filetypes=(("Comma Separated Values", "*.csv"), ("all files", "*.*")))
-    if not file_in:
-        input("Program Terminated. Press Enter to continue...")
-        exit()
-
-    return file_in
-
-
-def select_file_out(file_in):
-    file_out = asksaveasfilename(initialdir=file_in, title="Select file",
-                                 filetypes=(("Comma Separated Values", "*.csv"), ("all files", "*.*")))
-    if not file_out:
-        input("Program Terminated. Press Enter to continue...")
-        exit()
-
-    # Create an empty output file
-    open(file_out, 'a').close()
-
-    return file_out
-
-
-def y_n_question(question):
-    while True:
-        # Ask question
-        answer = input(question)
-        answer_cleaned = answer[0].lower()
-        if answer_cleaned == 'y' or answer_cleaned == 'n':
-            return answer_cleaned
-        else:
-            print("Invalid input, please try again.")
+    # Return completed PCA
+    return data_out, components_df
 
 
 def column_list(data, column_selection_type_in):
@@ -308,57 +288,6 @@ def column_selection_type(start_index: int):
         else:
             break
     return index_selection
-
-
-def open_unknown_csv(file_in, delimination):
-    encode_index = 0
-    encoders = ['utf_8', 'latin1', 'utf_16',
-                'ascii', 'big5', 'big5hkscs', 'cp037', 'cp424',
-                'cp437', 'cp500', 'cp720', 'cp737', 'cp775',
-                'cp850', 'cp852', 'cp855', 'cp856', 'cp857',
-                'cp858', 'cp860', 'cp861', 'cp862', 'cp863',
-                'cp864', 'cp865', 'cp866', 'cp869', 'cp874',
-                'cp875', 'cp932', 'cp949', 'cp950', 'cp1006',
-                'cp1026', 'cp1140', 'cp1250', 'cp1251', 'cp1252',
-                'cp1253', 'cp1254', 'cp1255', 'cp1256', 'cp1257',
-                'cp1258', 'euc_jp', 'euc_jis_2004', 'euc_jisx0213', 'euc_kr',
-                'gb2312', 'gbk', 'gb18030', 'hz', 'iso2022_jp',
-                'iso2022_jp_1', 'iso2022_jp_2', 'iso2022_jp_2004', 'iso2022_jp_3', 'iso2022_jp_ext',
-                'iso2022_kr', 'latin_1', 'iso8859_2', 'iso8859_3', 'iso8859_4',
-                'iso8859_5', 'iso8859_6', 'iso8859_7', 'iso8859_8', 'iso8859_9',
-                'iso8859_10', 'iso8859_11', 'iso8859_13', 'iso8859_14', 'iso8859_15',
-                'iso8859_16', 'johab', 'koi8_r', 'koi8_u', 'mac_cyrillic',
-                'mac_greek', 'mac_iceland', 'mac_latin2', 'mac_roman', 'mac_turkish',
-                'ptcp154', 'shift_jis', 'shift_jis_2004', 'shift_jisx0213', 'utf_32',
-                'utf_32_be', 'utf_32_le', 'utf_16', 'utf_16_be', 'utf_16_le',
-                'utf_7', 'utf_8', 'utf_8_sig']
-
-    data = open_file(file_in, encoders[encode_index], delimination)
-    while data is str:
-        if encode_index < len(encoders) - 1:
-            encode_index = encode_index + 1
-            data = open_file(file_in, encoders[encode_index], delimination)
-        else:
-            print("Can't find appropriate encoder")
-            exit()
-
-    return data
-
-def open_file(file_in, encoder, delimination):
-    try:
-        data = pd.read_csv(file_in, low_memory=False, encoding=encoder, delimiter=delimination)
-        print("Opened file using encoder: " + encoder)
-
-    except UnicodeDecodeError:
-        print("Encoder Error for: " + encoder)
-        return "Encode Error"
-    return data
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
